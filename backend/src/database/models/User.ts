540504c73594c809199e6716c67b111e12891225
victorhbm <victor.hugofla@hotmail.com>
2022-07-02 13:01:08 +0100

    feat: create User model

---
+++ backend/src/database/models/User.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Task from './Task';
+
+class User extends Model {
+  declare id: number;
+  declare name: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  sequelize: db,
+  tableName: 'Users',
+  timestamps: false,
+});
+
+User.hasMany(Task, { foreignKey: 'userId', as: 'userId' });
+Task.belongsTo(User, { foreignKey: 'userId', as: 'userId' });
+
+export default User;
