6a6ebe636592b3457444a5789b91cf0c6ec022b5
victorhbm <victor.hugofla@hotmail.com>
2022-07-04 16:20:25 +0100

    feat(TaskSchema):  create updateTaskStatusSchema method

--- backend/src/schemas/TaskSchema.ts
+++ backend/src/schemas/TaskSchema.ts
@@ -3,6 +3,12 @@ import * as Joi from 'joi';
 class TaskSchema {
   public createTaskSchema = Joi.object({
     task: Joi.string().required(),
+    user: Joi.object(),
+  });
+
+  public updateTaskStatusSchema = Joi.object({
+    status: Joi.string().valid('Pendente', 'Em andamento', 'Pronto').required(),
+    user: Joi.object(),
   });
 };
 
