543571893f90dabcdf1ba0157404cc95c313552a
victorhbm <victor.hugofla@hotmail.com>
2022-07-04 10:53:25 +0100

    feat: add endpoint POST /tasks to create a new task

--- backend/src/routes/TaskRouter.ts
+++ backend/src/routes/TaskRouter.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import TaskController from "../controllers/TaskController";
 import AuthMiddleware from "../middlewares/AuthMiddleware";
+import TaskMiddleware from "../middlewares/TaskMiddleware";
 
 const router = Router();
 
 router.get('/',
   AuthMiddleware,
   TaskController.getTasksByUserId
+);
+
+router.post('/',
+  TaskMiddleware.taskCreateBodyValidate,
+  AuthMiddleware,
+  TaskController.createTask
 );
 
 export default router;
