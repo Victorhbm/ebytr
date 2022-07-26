2b52c9815b050e90827cdd3c07cc3d60596df92a
Victorhbm <victor.hugofla@hotmail.com>
2022-07-26 22:21:34 +0100

    refactor(app): to use BrowserRouter and check logged users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AppContext from './context/AppContext.js';
+import Login from './pages/Login.jsx';
+import Main from './pages/Main.jsx';
+import Register from './pages/Register.jsx';
 
 function App() {
+  const { setUserData } = useContext(AppContext);
+  const [isLogged, setIsLogged] = useState(true);
+
+  const getUserData = useCallback(() => {
+    const getUserFromLocalStorage = JSON.parse(localStorage.getItem('user'));
+
+    if (getUserFromLocalStorage) {
+      setUserData(getUserFromLocalStorage);
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }, [setUserData]);
+
+  useEffect(() => {
+    getUserData();
+  }, [getUserData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/register" element={ <Register /> } />
+        <Route exact path="/login" element={ <Login /> } />
+        <Route exact path="/" element={
+          isLogged
+            ? <Main />
+            : <Navigate to="/login" />
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
