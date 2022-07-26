2ec7b314ffc4c96d2e1ea59b01d886de5e4ae123
Victorhbm <victor.hugofla@hotmail.com>
2022-07-26 22:10:37 +0100

    refactor: change DB name

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'ebytr',
+  database: process.env.DB_NAME || 'todo-list',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
