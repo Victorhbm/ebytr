a5a850e6732d712a4f54d113fa113e033661c5f3
Victorhbm <victor.hugofla@hotmail.com>
2022-07-28 17:58:00 +0100

    feat: create Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AppContext from "../context/AppContext";
+
+function Header() {
+  const { userData: { name } } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    navigate('/login');
+  };
+
+  return (
+    <header>
+      <h1>To-do List</h1>
+      <div>
+        <p>{ name }</p>
+        <button type="button" onClick={ logout }>
+          Log Out
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
