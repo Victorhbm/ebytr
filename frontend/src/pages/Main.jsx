e1b31c5cf0a78639d21c6baacd85078f3e6b9992
Victorhbm <victor.hugofla@hotmail.com>
2022-07-29 11:47:30 +0100

    feat: add addTaskForm and TasksTable to main page

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,9 +1,13 @@
+import AddTaskForm from "../components/AddTaskForm";
 import Header from "../components/Header";
+import TasksTable from "../components/TasksTable";
 
 function Main() {
   return (
     <>
       <Header />
+      <AddTaskForm />
+      <TasksTable />
     </>
   )
 }
