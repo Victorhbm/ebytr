d4e519b4cc98e01f88546d42ae2887689df6b09f
Victorhbm <victor.hugofla@hotmail.com>
2022-07-29 11:39:47 +0100

    feat: create update task name joi schema

--- backend/src/schemas/TaskSchema.ts
+++ backend/src/schemas/TaskSchema.ts
@@ -6,8 +6,13 @@ class TaskSchema {
     user: Joi.object(),
   });
 
-  public updateTaskStatusSchema = Joi.object({
-    status: Joi.string().valid('Pendente', 'Em andamento', 'Pronto').required(),
+  public updateStatusSchema = Joi.object({
+    status: Joi.string().valid('To do', 'In Progress', 'Done').required(),
+    user: Joi.object(),
+  });
+
+  public updateTaskNameSchema = Joi.object({
+    task: Joi.string().required(),
     user: Joi.object(),
   });
 };
