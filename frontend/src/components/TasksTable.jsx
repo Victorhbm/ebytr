609940ff440758f788ab3152413988f812a893d8
Victorhbm <victor.hugofla@hotmail.com>
2022-09-23 17:27:34 +0100

    refactor: remove Sort section

--- frontend/src/components/TasksTable.jsx
+++ frontend/src/components/TasksTable.jsx
@@ -1,12 +1,10 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect } from "react";
 import AppContext from "../context/AppContext";
 import { getAllTasks } from "../services/apiRequests";
 import TableItem from "./TableItem";
 
 function TasksTable() {
-  const { userData, setTasks, tasks, filter } = useContext(AppContext);
-  const [sortColumn, setSortColumn] = useState('id');
-  const [sortOrder, setSortOrder] = useState('ASC');
+  const { userData, setTasks, tasks, filter, sortColumn, sortOrder } = useContext(AppContext);
 
   const getTasks = useCallback(async () => {
     if (userData.token) {
@@ -24,63 +22,24 @@ function TasksTable() {
     return sortedTasks;
   }
 
-  const changeSort = (col) => {
-    setSortColumn(col);
-    setSortOrder(sortOrder === 'ASC' ? 'DESC' : 'ASC');
-  };
-
   useEffect(() => {
     getTasks();
   }, [getTasks]);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Task
-            <button
-              onClick={ () => changeSort('task') }
-            >
-              {sortColumn === 'task' && sortOrder === 'DESC'
-                ? '▼'
-                : '▲'}
-            </button>
-          </th>
-          <th>
-            Status
-            <button onClick={ () => changeSort('status') }>
-              {sortColumn === 'status' && sortOrder === 'DESC'
-                ? '▼'
-                : '▲'}
-            </button>
-          </th>
-          <th>
-            Created At
-            <button onClick={ () => changeSort('createdAt') }>
-              {sortColumn === 'createdAt' && sortOrder === 'DESC'
-                ? '▼'
-                : '▲'}
-            </button>
-          </th>
-          <th>Edit</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasks.length > 0 && sortTasks()
-          .filter((t) => !filter || t.status === filter)
-          .map(({ id, task, status, createdAt }) => (
-            <TableItem
-              key={ id }
-              id={ id }
-              task={ task }
-              status={ status }
-              createdAt={ createdAt }
-            />
-          ))}
-      </tbody>
-    </table>
+    <section>
+      {tasks.length > 0 && sortTasks()
+        .filter((t) => !filter || t.status === filter)
+        .map(({ id, task, status, createdAt }) => (
+          <TableItem
+            key={ id }
+            id={ id }
+            task={ task }
+            status={ status }
+            createdAt={ createdAt }
+          />
+        ))}
+    </section>
   );
 }
 
