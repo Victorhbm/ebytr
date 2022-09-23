56206b98723890c899ee0de69e02256e210211b5
Victorhbm <victor.hugofla@hotmail.com>
2022-09-23 17:23:23 +0100

    style: add style to Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import AppContext from "../context/AppContext";
+// import { MdLogout } from "react-icons/md"
 
 function Header() {
   const { userData: { name }, setUserData } = useContext(AppContext);
@@ -13,11 +14,11 @@ function Header() {
   };
 
   return (
-    <header>
-      <h1>To-do List</h1>
-      <div>
-        <p>{ name }</p>
-        <button type="button" onClick={ logout }>
+    <header className="flex justify-between px-5 py-3 bg-primary items-center">
+      <h1 className="text-2xl font-bold">To-do List</h1>
+      <div className="flex justify-between items-center">
+        <p className="mr-3 text-lg min-h-8">{ name }</p>
+        <button type="button" className="btn btn-outline btn-sm" onClick={ logout }>
           Log Out
         </button>
       </div>
